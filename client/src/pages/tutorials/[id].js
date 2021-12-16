04e15a203cb7f93dd9ee6c0e46c52a9d0bdec172
SauLuoiBo <86532824+SauLuoiBo@users.noreply.github.com>
2021-12-16 15:40:32 +0700

    ec

--- client/src/pages/tutorials/[id].js
+++ client/src/pages/tutorials/[id].js
@@ -153,7 +153,7 @@ const Work = () => {
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullscreen
+                  allowFullScreen
                 ></iframe>
               </PlanWrapper>
             </AnswerPlan>
